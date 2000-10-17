d4fc67544aa553c0932e9dedd0bc9c3b3e3152c2
Sam Lantinga <slouken@libsdl.org>
2000-10-17 03:36:11 +0000

    Add architecture, version, and OS information to the scripts

--- image/pre-patch.sh
+++ image/pre-patch.sh
@@ -2,6 +2,6 @@
 #
 # This script is executed before the patch is applied
 
-: echo "This is a pre-patch script for $1 installed in $2"
+: echo "This is a pre-patch script for $1 installed in $3"
 
 exit 0
