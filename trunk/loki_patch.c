0701d0b48bc2ac6ea6861e6724349271d83d41be
hercules <hercules@3590d2f8-701e-0410-9151-f68df3d4a0bd>
2000-10-30 23:35:15 +0000

    A more flexible way of handling patches that update several versions of a product

--- trunk/loki_patch.c
+++ trunk/loki_patch.c
@@ -144,14 +144,11 @@ static void update_registry(loki_patch *patch)
       loki_split_version(patch->version,
                          new_versionbase, sizeof(new_versionbase),
                          new_versionext, sizeof(new_versionext));
-      if ( strcmp(old_versionext, new_versionext) != 0 ) {
-          /* Must be a version flavor change, don't update the base version.
-             The auto-update tool enforces this rule; you can update either
-             the flavor or the base of a version, but not both.  This code
-             has been added to allow patch to switch the flavor of multiple
-             base versions of a product.
-           */
+      if ( strcmp(new_versionbase, "*") == 0 ) {
           strcpy(new_versionbase, old_versionbase);
+      }
+      if ( strcmp(new_versionext, "*") == 0 ) {
+          strcpy(new_versionext, old_versionext);
       }
       sprintf(new_version, "%s%s", new_versionbase, new_versionext);
       loki_setversion_component(component, new_version);
